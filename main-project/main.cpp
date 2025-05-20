b2c4ea9245b913941446318e30371f5666177fa8
Vlad Pichyov <qwerd356@gmail.com>
2025-05-20 23:52:00 +0300

    Реализовано форматирование данных при выводе в консоль

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
@@ -10,27 +11,44 @@ int main()
 {
     setlocale(LC_ALL, "Russian");
     cout << "Лабораторная работа №8. GIT\n";
-    cout << "Вариант №1. результат марафона\n";
-    cout << "Автор: Пичев Владислав\n";
+    cout << "Вариант №1. Результат марафона\n";
+    cout << "Автор: Пичев Владислав\n\n";
+
     runner_subscription* subscriptions[MAX_FILE_ROWS_COUNT];
     int size;
+
     try
     {
         read("data.txt", subscriptions, size);
+        cout << "***** Результаты марафона *****\n\n";
+
         for (int i = 0; i < size; i++)
         {
-            cout << subscriptions[i]->runner.last_name;
-            cout << subscriptions[i]->runner.first_name;
-            cout << subscriptions[i]->runner.middle_name << " ";
-            cout << subscriptions[i]->start.hour << ' ';
-            cout << subscriptions[i]->start.minute << ' ';
-            cout << subscriptions[i]->start.second << " ";
-            cout << subscriptions[i]->finish.hour << ' ';
-            cout << subscriptions[i]->finish.minute << ' ';
-            cout << subscriptions[i]->finish.second << " ";
+            /********** вывод бегуна **********/
+            cout << "Бегун...........: ";
+            cout << subscriptions[i]->runner.last_name << " ";
+            cout << subscriptions[i]->runner.first_name[0] << ". ";
+            cout << subscriptions[i]->runner.middle_name[0] << ".\n";
+
+            /********** вывод клуба **********/
+            cout << "Клуб............: ";
             cout << subscriptions[i]->club << '\n';
+
+            /********** вывод времени старта **********/
+            cout << "Время старта....: ";
+            cout << setw(2) << setfill('0') << subscriptions[i]->start.hour << ':';
+            cout << setw(2) << setfill('0') << subscriptions[i]->start.minute << ':';
+            cout << setw(2) << setfill('0') << subscriptions[i]->start.second << '\n';
+
+            /********** вывод времени финиша **********/
+            cout << "Время финиша....: ";
+            cout << setw(2) << setfill('0') << subscriptions[i]->finish.hour << ':';
+            cout << setw(2) << setfill('0') << subscriptions[i]->finish.minute << ':';
+            cout << setw(2) << setfill('0') << subscriptions[i]->finish.second << '\n';
+
             cout << '\n';
         }
+
         for (int i = 0; i < size; i++)
         {
             delete subscriptions[i];
@@ -40,5 +58,6 @@ int main()
     {
         cout << error << '\n';
     }
+
     return 0;
 }
