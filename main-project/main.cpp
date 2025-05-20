467f46313a5dd77a17e84dd7e60c0d36777de684
Vlad Pichyov <qwerd356@gmail.com>
2025-05-20 23:57:01 +0300

    Добавлены тестовые данные

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -7,5 +7,6 @@ int main()
     cout << "Laboratory work #8. GIT\n";
     cout << "Variant #1. marathon results\n";
     cout << "Author: Pichyov Vladislav\n";
+    cout << "group: 24ISiT1d";
     return 0;
 }
